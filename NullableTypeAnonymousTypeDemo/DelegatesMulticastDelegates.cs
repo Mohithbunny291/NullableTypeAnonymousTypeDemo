21f0768e1207845969b7f3cfd83e704312797965
Mohith Reddy <Mohithbunny291@gmail.com>
2024-02-16 10:56:32 +0530

    Add project files.

---
+++ NullableTypeAnonymousTypeDemo/DelegatesMulticastDelegates.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NullableTypeAnonymousTypeDemo
+{
+    internal class DelegatesMulticastDelegates
+    {
+        public int Add(int a, int b)
+        {
+            Console.WriteLine("Executing Add Method");
+            return a + b;
+        }
+
+        public int Subtract(int a, int b) {  return a - b; }
+    }
+}
---
+++ NullableTypeAnonymousTypeDemo/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NullableTypeAnonymousTypeDemo
+{
+    internal class Program
+    {
+        delegate int Calculate(int first, int second);
+
+        
+        static void Main(string[] args)
+        {
+            //value type can be assigned as null 
+            //reference type can not be null
+
+            int? i = null;
+            bool? flag = null;
+
+            if (flag.HasValue)
+            {
+                bool test = flag.Value;
+            }
+        
+
+            //Anonymous Types
+
+            var anonymousTypeVariable = new{ CompanyName = "ABC Corp", Year = 1998 };
+            
+            Console.WriteLine(i);
+            Console.WriteLine(anonymousTypeVariable.CompanyName);
+            Console.WriteLine(anonymousTypeVariable.Year);
+
+            var iSample = 10;
+            var strSample = "John";
+
+            
+
+
+
+            //  DelegatesMulticastDelegates
+            DelegatesMulticastDelegates delegatesMulticastDelegates = new DelegatesMulticastDelegates();
+            int i1 = 20, i2 = 30;
+            Calculate calculate = new Calculate(delegatesMulticastDelegates.Add);
+            // above or this are same Calculate calculate = Add;
+
+            Calculate calculateSub = new Calculate(delegatesMulticastDelegates.Subtract);
+            // above or this are same Calculate calculateSub = Subtract;
+
+            int result = calculate(i1, i2);
+            // int result = calculate.invoke(i1, i2);
+
+            Console.WriteLine(result);
+
+
+
+
+
+            
+
+        }
+    }
+}
